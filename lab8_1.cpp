25ea4258cb1816b7b44e2b9c230f166c2bff22ca
Athikan <athikan_thoyaem@cmu.ac.th>
2024-12-09 22:22:17 +0700

    "lab8_1_complete"

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -3,15 +3,33 @@ using namespace std;
 
 int main()
 {
-  char rank;
+  char rank ; 
   cout << "Input your rank: ";
   cin >> rank;
-  switch(rank){
-    case 'S': cout << "You have received Super Ultra Rare Unit!!!\n";
-    case 'A': cout << "You have received 5 gems.\n";
-    case 'B': cout << "You have received 1 gems.\n";
-    case 'C': cout << "You have received 2000 coins.\n";
-    case 'D': cout << "You have received very KAK items.\n";
+  if(rank == 's' || rank == 'S'){
+    cout <<  "You have received Super Ultra Rare Unit!!!\n";
+    cout << "You have received 5 gems.\n";
+    cout << "You have received 1 gems.\n";
+    cout << "You have received 2000 coins.\n";
+    cout << "You have received very KAK items.\n";
   }
+        if(rank == 'A' || rank == 'a'){
+          cout << "You have received 5 gems.\n";
+          cout << "You have received 1 gems.\n";
+          cout << "You have received 2000 coins.\n";
+          cout << "You have received very KAK items.\n";
+        }
+        if(rank == 'B' || rank == 'b'){
+           cout << "You have received 1 gems.\n";
+           cout << "You have received 2000 coins.\n";
+           cout << "You have received very KAK items.\n";
+        }
+           if(rank =='C' || rank == 'c'){
+            cout << "You have received 2000 coins.\n";
+            cout << "You have received very KAK items.\n";
+           }
+            if(rank =='D' || rank == 'd'){
+            cout << "You have received very KAK items.\n";
+        }
   return 0;
 }
